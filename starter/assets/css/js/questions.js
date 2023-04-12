44fa288ce08f9f80b3ddd32eeb7f90425d426e38
Amaka Metu <103032546+Oliviamum@users.noreply.github.com>
2023-04-12 22:57:33 +0100

    my second commit

---
+++ starter/assets/css/js/questions.js
@@ -0,0 +1,52 @@
+var questions = [
+    {
+        title: "Which keyword is used to declare a variable in javascript?",
+        choices: ["var", "let", "const", "both a and b"],
+        answer: "both a and b"
+    },
+    {
+        title: "What is the result of 3 + 2 + '7' in JavaScript?",
+        choices: ["12", "327", "57", "NaN"],
+        answer: "57"
+    },
+    {
+        title: "Which method can be used to remove the last element from an array in JavaScript?",
+        choices: ["pop()", "shift()", "splice()", "slice()"],
+        answer: "pop()"
+    },
+    {
+        title: "What is the output of console.log(typeof []) in JavaScript?",
+        choices: ["object", "array", "undefined", "string"],
+        answer: "object"
+    }
+    {
+        title: "What is the purpose of the 'use strict' directive in JavaScript?",
+        choices: ["To enable strict mode", "To disable strict mode", "To allow silent errors", "To throw more exceptions"],
+        answer: "To enable strict mode"
+    },
+    {
+        title: "What is the difference between '==' and '===' in JavaScript?",
+        choices: ["'==' compares value and type, while '===' compares only type", "'==' compares only type, while '===' compares value and type", "'==' compares value only, while '===' compares value and type", "'==' and '===' are interchangeable"],
+        answer: "'==' compares value only, while '===' compares value and type"
+    },
+    {
+        title: "Which function is used to execute a piece of code after a specified time interval in JavaScript?",
+        choices: ["setTimeout()", "setInterval()", "clearTimeout()", "clearInterval()"],
+        answer: "setTimeout()"
+    }  
+    {
+        title: "What is the output of console.log(0.1 + 0.2 === 0.3) in JavaScript?",
+        choices: ["true", "false", "undefined", "NaN"],
+        answer: "false"
+    }
+    {
+        title: "Which method can be used to join two or more arrays in JavaScript?",
+        choices: ["concat()", "push()", "join()", "slice()"],
+        answer: "concat()"
+    }
+    {
+        title: "What is the purpose of the 'this' keyword in JavaScript?",
+        choices: ["To refer to the current object", "To refer to the global object", "To create a new object", "To delete an object"],
+        answer: "To refer to the current object"
+    }
+];
---
+++ starter/assets/css/js/scores.js
@@ -0,0 +1,23 @@
+const highscoresList = document.getElementById("highscores");
+
+//Get highscores dat from local storage
+const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+//sort highscores in descending order
+highscores.sort((a, b) => b.score - a.score);   
+
+//displayhighscores in HTML
+highscores.forEach(function(score) {
+    const li = document.createElement("li");
+    li.textContent = `${score.initials}: ${score.score}`;
+    highscoresList.appendChild(li);
+});
+
+//Clear highscores list when button is clicked
+const clearButton = document.getElementById("clear");
+clearButton.addEventListener("click",()=> {
+    localsStorage.removeItem("highscores");
+    highscoresList.innerHTML = "";
+});
+
+
